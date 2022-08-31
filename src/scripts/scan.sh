551072d444f04df7d64aa40fa9b9b658a15d9eee
focaloid-dev <alshoja@focaloid.com>
2022-08-31 19:38:40 +0530

    fix: dev version

--- src/scripts/scan.sh
+++ src/scripts/scan.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo Startin Slim CLI installation 
+echo Starting Slim CLI installation 
 curl -sL https://raw.githubusercontent.com/docker-slim/docker-slim/master/scripts/install-dockerslim.sh | sudo -E bash -
 
 echo Performing a xray scan over Image: "${PARAM_IMAGE}"
